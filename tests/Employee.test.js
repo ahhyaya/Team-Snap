49611eddaa1a19462039f3dce209fd0109dcbf40
doldol123 <toruig59@gmail.com>
2022-10-22 16:18:32 -0700

    complete Employee.js

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,17 +1,29 @@
 // require Employee
 const Employee = require('../lib/Employee');
+const employee = new Employee();
+const testName = 'Rui'; 
+const testId = 123;
+const testEmail = 'toruig59@gmail.com';
 
 // test setting name 
 test('Set Employee Name', () => {
-    const name = 'Rui';
-    const employee = new Employee(name);
-    expect(employee.name).toBe(name);
+    expect(employee.name).toBe(testName);
 })
+
 // test setting ID
+test('Set Employee ID', () => {
+    expect(employee.id).toBe(testId);
+})
 
 // test setting email
+test('Set Employee Email', () => {
+    expect(employee.email).toBe(testEmail);
+})
 
 // test getting name
+test('Get Employee Name', () => {
+    expect(employee.getName()).toBe(testName);
+})
 
 // test getting ID
 
