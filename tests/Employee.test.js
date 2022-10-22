783bd200cfe36430ebbd0f5ed3c4ea8f527783be
doldol123 <toruig59@gmail.com>
2022-10-22 15:58:29 -0700

    create files in lib

---
+++ tests/Employee.test.js
@@ -0,0 +1,15 @@
+// require Employee
+const Employee = require('../lib/Employee')
+// test setting name 
+
+// test setting ID
+
+// test setting email
+
+// test getting name
+
+// test getting ID
+
+// test getting email
+
+// test getting Role of Emploee
