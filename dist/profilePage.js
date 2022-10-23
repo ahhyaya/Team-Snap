79b4b3ac2e141b3d23fa1a8734a9c3ae8029a5fc
doldol123 <toruig59@gmail.com>
2022-10-22 23:19:35 -0700

    format

--- dist/profilePage.js
+++ dist/profilePage.js
@@ -84,32 +84,5 @@ const generateProfilePage = (employeeCard) => {
     `;
 }
 
-// var cardName = $('.card-header');
-// var cardRole = $('.card-footer')
-
-
-// cardName.attr('id','employee-name');
-// cardRole.attr('id', 'department')
-
-// cardName.append()
-
-// const profilePage = (input)=> {
-
-
-// switch (input.employee) {
-//     case 'Manager':
-//         employee.push(new Manager(input.name, input.id, input.email, input.dept));
-//         break;
-//     case 'Engineer':
-//         employee.push(new Engineer(input.name, input.id, input.email, input.github));
-//         break;
-//     case 'Intern':
-//         employee.push(new Intern(input.name, input.id, input.email, input.school));
-//         break;
-// }
-// }
-
-
-
 
 module.exports = profilePage;
