f7d73671d0266865c9d34376cb07e7512646a32e
doldol123 <toruig59@gmail.com>
2022-10-23 12:54:46 -0700

    final adjustment

--- dist/profilePage.js
+++ dist/profilePage.js
@@ -90,6 +90,7 @@ const generateProfilePage = (employeeCard) => {
         <link rel="stylesheet" href=
 "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
         <script src="https://cdn.tailwindcss.com"></script>
+        <link rel="stylesheet" href="../src/styles.css">
     <title>Team Snap</title>
 </head>
 <body>
