3562d1fd8ac74224237dbcd9c7d70279c06e26e7
doldol123 <toruig59@gmail.com>
2022-10-22 21:20:07 -0700

    complete lib

--- dist/profilePage.js
+++ dist/profilePage.js
@@ -1,2 +1,31 @@
+const employee = require("../lib/Employee");
+
+var cardName = $('.card-header');
+var cardRole = $('.card-footer')
 
 
+cardName.attr('id','employee-name');
+cardRole.attr('id', 'department')
+
+cardName.append()
+
+const profilePage = (input)=> {
+
+
+    // switch (input.employee) {
+    //     case 'Manager':
+    //         employee.push(new Manager(input.name, input.id, input.email, input.dept));
+    //         break;
+    //     case 'Engineer':
+    //         employee.push(new Engineer(input.name, input.id, input.email, input.github));
+    //         break;
+    //     case 'Intern':
+    //         employee.push(new Intern(input.name, input.id, input.email, input.school));
+    //         break;
+    // }
+}
+
+
+
+
+module.exports = profilePage;
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // include packages needed
-const fs = require ('fs');
+const fs = require('fs');
 const inquirer = require('inquirer');
 const profilePage = require('./dist/profilePage')
 const Employee = require('./lib/Employee');
@@ -7,51 +7,78 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 const { default: Choices } = require('inquirer/lib/objects/choices');
-const infoQuestions =[];
+const team = [];
+// let infoQuestions = [];
 // create an array of questions for user input
-const addEmployeeQuestions = () => {
-    console.log(`Add employee's info to the team`);
-    infoQuestions = [
-    {
-        type: 'list',
-        name: 'role',
-        message: 'Please choose employee\'s role: ',
-        choices: ['Manager', 'Engineer', 'Intern', 'None']
-    },
-    {
-        type: 'input',
-        name: 'name',
-        message: 'Please enter employee\'s name: '
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: 'Please enter employee\'s ID number: '
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: 'Please enter employee\'s ID number: '
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Please enter employee\'s ID email: '
-    }
-]
-}
+// const addEmployee = () => {
+    console.log(
+        `
+=======================================
+    Add employee's info to the team
+---------------------------------------
+         `);
+    const infoQuestions = [
+        {
+            type: 'list',
+            name: 'role',
+            message: 'Please choose employee\'s role: ',
+            choices: ['Manager', 'Engineer', 'Intern', 'None']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Please enter employee\'s name: '
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Please enter employee\'s ID number: '
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter employee\'s email: '
+        }
+    ]
+// }
+
+
 // function to write Team file
-const addToTeam = (input) => {
+const addToTeam = (data) => {
     let teamPage = './dist/index.html';
-    fs.writeFileSync(teamPage, input, (err) => {
-        err ? console.log(err) : console.log ('Team profile is ready!');
-    })
+    fs.writeFileSync(teamPage, data, (err) => {
+        err ? console.log(err) : console.log('Team profile is ready!');
+    });
 }
 // function to initialize app
-let init = () => {
-    inquirer.prompt(addEmployeeQuestions)
-    .then(answers => addToTeam(addEmployeeQuestions()));
+const init = () => {
+    inquirer.prompt(infoQuestions)
+        .then(answers => addToTeam(profilePage(answers))) 
+        .then(emaployeeData => {
+            let { name, id, email, role, github, school } = emaployeeData;
+            let employee;
+            if (role === 'Manager') {
+                employee = new Manager(name, id, email, officeNumber);
+                console.log(employee)
+            } else if (role === 'Engineer') {
+                employee = new Engineer(name, id, email, github);
+                console.log(employee)
+            } else if (role === 'Intern') {
+                employee = new Intern(name, id, email, school);
+                console.log(employee)
+            }
+            team.push(employee);
+            return team;
+        });
+          
 }
 // function to call to init app
+
+// addToTeam()
+// .then(addEmployee)
+// .then(team => {
+//     return profilePage(team);
+// })
+
 
 init();
