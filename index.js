b4a78dca084f60dac6079034963b0b18ae5db041
doldol123 <toruig59@gmail.com>
2022-10-22 22:25:08 -0700

    fix index.js according to challenge requirment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // include packages needed
 const fs = require('fs');
 const inquirer = require('inquirer');
-const profilePage = require('./dist/profilePage')
+// const profilePage = require('./dist/profilePage')
 const Employee = require('./lib/Employee');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
@@ -10,43 +10,114 @@ const { default: Choices } = require('inquirer/lib/objects/choices');
 const team = [];
 // let infoQuestions = [];
 // create an array of questions for user input
-// const addEmployee = () => {
-    console.log(
-        `
+const addManager = () => {
+   
+console.log(
+    `
 =======================================
-    Add employee's info to the team
+    Enter Manager's info to start
 ---------------------------------------
          `);
-    const managerInfo = [
-        // {
-        //     type: 'list',
-        //     name: 'role',
-        //     message: 'Please choose employee\'s role: ',
-        //     choices: ['Manager', 'Engineer', 'Intern', 'None']
-        // },
-        {
-            type: 'input',
-            name: 'name',
-            message: 'Please enter manager\'s name: '
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: 'Please enter manager\'s  ID number: '
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'Please enter manager\'s  email: '
-        },
-        {
-            type: 'input',
-            name: 'officeNumber',
-            message: 'Please enter manager\'s office number: '
-        }
-    ]
-// }
 
+         return inquirer.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Please enter manager\'s name: '
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'Please enter manager\'s  ID number: '
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter manager\'s  email: '
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'Please enter manager\'s office number: '
+    }
+]
+
+
+)}
+
+const addEmployee = () => {
+
+console.log(
+    `
+=======================================
+Please Enter Employee's info 
+---------------------------------------
+     `);
+return inquirer.prompt([
+    {
+        type: 'list',
+        name: 'role',
+        message: 'Please select employee\'s role: ',
+        choices: ['Engineer', 'Intern']
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Please enter employee\'s name: '
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'Please enter employee\'s  ID number: '
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter employee\'s  email: '
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter employee\'s Github username: ',
+        when: (input) => input.role === 'Engineer',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter employee\'s Github username: ')
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: 'Please enter employee\'s school: ',
+        when: (input) => input.role === 'Intern',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter employee\'s school: ')
+            }
+        }
+    }
+
+])
+.then(employeeData => {
+    let { name, id, email, role, github, school } = employeeData;
+    let employee;
+
+    if(role === 'Engineer') {
+        employee = new Engineer(name, id, email, github);
+        console.log(employee)
+    } else if (role === 'Intern') {
+        employee = new Intern(name, id, email, school);
+        console.log(employee)
+    }
+
+    team.push(employee);
+})
+}
 
 // function to write Team file
 const addToTeam = (data) => {
@@ -56,27 +127,37 @@ const addToTeam = (data) => {
     });
 }
 // function to initialize app
-const init = () => {
-    inquirer.prompt(infoQuestions)
-        .then(answers => addToTeam(profilePage(answers))) 
-        .then(emaployeeData => {
-            let { name, id, email, role, github, school } = emaployeeData;
-            let employee;
-            if (role === 'Manager') {
-                employee = new Manager(name, id, email, officeNumber);
-                console.log(employee)
-            } else if (role === 'Engineer') {
-                employee = new Engineer(name, id, email, github);
-                console.log(employee)
-            } else if (role === 'Intern') {
-                employee = new Intern(name, id, email, school);
-                console.log(employee)
-            }
-            team.push(employee);
-            return team;
-        });
-          
-}
+// const init = () => {
+//     inquirer.prompt(managerInfo)
+//         .then(managerInput => {
+//             const { name, id, email, officeNumber } = managerInput;
+//             const manager = new Manager(name, id, email, officeNumber);
+//             team.push(manager);
+//             console.log(manager)
+//         })
+       
+// }
+
+
+
+// .then(emaployeeData => {
+//     let { name, id, email, role, github, school } = emaployeeData;
+//     let employee;
+//     if (role === 'Manager') {
+//         employee = new Manager(name, id, email, officeNumber);
+//         console.log(employee)
+//     } else if (role === 'Engineer') {
+//         employee = new Engineer(name, id, email, github);
+//         console.log(employee)
+//     } else if (role === 'Intern') {
+//         employee = new Intern(name, id, email, school);
+//         console.log(employee)
+//     }
+//     team.push(employee);
+//     return team;
+// });
+
+
 // function to call to init app
 
 // addToTeam()
@@ -86,4 +167,12 @@ const init = () => {
 // })
 
 
-init();
+// {
+//     type: 'list',
+//     name: 'role',
+//     message: 'Please choose employee\'s role: ',
+//     choices: ['Manager', 'Engineer', 'Intern', 'None']
+// },
+
+// init();
+addManager()
